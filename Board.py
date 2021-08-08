26235d65717f7e8cef90b5294d07ecebf4324be9
NateRiz <nathan.rizik@gmail.com>
2021-08-07 22:40:33 -0700

    optims

--- Board.py
+++ Board.py
@@ -22,25 +22,41 @@ class Board:
         self.board = []
         self.pairs = {}
         self.load_board_from_file(file_name)
-        self.ids = set(self.pairs.keys())
+        self.ids = list(self.pairs.keys())
 
     def solve_board(self):
-        for char in self.ids:
-            point = self.pairs[char][0]
-            self.solve_pair(point.coord)
+        char = self.ids.pop()
+        point = self.pairs[char][0]
+        self.solve_pair(point)
+        self.ids.append(char)
 
-    def solve_pair(self, coord):
+    def solve_pair(self, point):
+        self._solve_pair(point, point.pair, point.coord)
+
+    def _solve_pair(self, src, dst, coord):
         print(self)
         dirs = ((-1, 0), (0, -1), (1, 0), (0, 1))
         for i, j in dirs:
             y = coord[0] + j
             x = coord[1] + i
             if 0 <= x < len(self.board) and 0 <= y < len(self.board):
-                if self.board[y][x] is None:
-                    self.board[y][x] = self.board[coord[0]][coord[1]]
-                    self.solve_pair((y, x))
-                    self.board[y][x] = None
+                if (y, x) == dst.coord:
+                    self.solve_board()
+                elif self.board[y][x] is None:
+                    error = False
+                    for n, m in dirs:
+                        a, b = y + m, x + n
+                        if a < 0 or a >= len(self.board) or b < 0 or b >= len(self.board):
+                            continue
+                        if self.board[a][b] == src.id and (a, b) != coord and (a, b) != dst.coord:
+                            error = True
+                            break
 
+                    if error:
+                        continue
+                    self.board[y][x] = src.id
+                    self._solve_pair(src, dst, (y, x))
+                    self.board[y][x] = None
 
     def load_board_from_file(self, file_name):
         path = os.path.join(os.getcwd(), "Boards", file_name)
@@ -73,5 +89,5 @@ class Board:
     def __str__(self):
         res = ""
         for row in self.board:
-            res += "".join([str(i) if i else "_" for i in row]) + "\n"
+            res += " ".join([str(i) if i else "_" for i in row]) + "\n"
         return res
